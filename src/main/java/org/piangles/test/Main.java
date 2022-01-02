9bdc257238d3d0414ea08e04624775f894c6d638
VamsiRR <68519686+VamsiRR@users.noreply.github.com>
2022-01-02 11:41:37 -0500

    Update Main.java
    Test

--- src/main/java/org/piangles/test/Main.java
+++ src/main/java/org/piangles/test/Main.java
@@ -5,7 +5,7 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.out.println("Maven central repo post test");
+		System.out.println("Maven central repo post test..");
 
 	}
 
